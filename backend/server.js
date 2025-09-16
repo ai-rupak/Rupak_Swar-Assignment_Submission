97989566fae6383a1e483ac3dfdc0ecd98f6236f
Rupak swar <116864759+ai-rupak@users.noreply.github.com>
2025-09-16 09:58:32 +0530

    deployment config 🚀

--- backend/server.js
+++ backend/server.js
@@ -28,5 +28,5 @@ app.get("/", (req, res) => {
     res.send("API is running...");
 });
 
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+// const PORT = process.env.PORT || 5000;
+// app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
